ff7059323b7ff26e9f9b261fdd3b6a96e73ed40e
Alexei Eraskin <levrun1@gmail.com>
2015-12-23 23:55:05 +0100

    HW3: add DDL and DML for meals entity

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,3 +11,32 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-23', 'breakfast', 400);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-23', 'lunch', 1400);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-23', 'dinner', 2400);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-21', 'breakfast', 300);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-21', 'lunch', 1200);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2015-12-21', 'dinner', 1400);
+
+
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100001, '2015-12-23', 'breakfast', 400);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100001, '2015-12-23', 'lunch', 1400);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100001, '2015-12-23', 'dinner', 2400);
