f863e3c3627c0114368151d2084844f6cec90e42
Alexei Eraskin <levrun1@gmail.com>
2016-01-21 00:57:01 +0100

    HW07: rest test for delete and update meal

--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -1,10 +1,21 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.TestUtil;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,6 +26,9 @@ public class UserMealRestControllerTest extends AbstractControllerTest {
 
     public static final String REST_URL = UserMealRestController.REST_URL + '/';
 
+    @Autowired
+    private UserMealService service;
+
     @Test
     public void testGet() throws Exception {
         TestUtil.print(mockMvc.perform(get(REST_URL + MEAL1_ID))
@@ -22,6 +36,26 @@ public class UserMealRestControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MATCHER.contentMatcher(MEAL1)));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1_ID).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), service.getAll(UserTestData.USER_ID));
+    }
+
+
+    @Test
+    public void testUpdate() throws Exception {
+        UserMeal updated = MealTestData.getUpdated();
+        mockMvc.perform(put(REST_URL + MEAL1_ID).contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        MATCHER.assertEquals(updated, service.get(MEAL1_ID, UserTestData.USER_ID));
     }
 
 
